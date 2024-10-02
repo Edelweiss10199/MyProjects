9ac8a8148703c8ba483fb9385164c93f3732006e
Edelweiss10199 <denver821@mail.ru>
2024-10-02 23:01:21 +0300

    Update module_3_1.py

--- module3/module_3_1.py
+++ module3/module_3_1.py
@@ -1,1 +1,25 @@
+calls = 0
+def count_calls():
+
+    global calls
+    calls += 1
+
+
+def string_info(string1: str):
+    count_calls()
+
+    return len(string1), string1.upper(), string1.lower()
+
+
+def is_contains(string2: str, list_to_search: list):
+    count_calls()
+    if string2 in list_to_search:
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))  # (8, 'CAPYBARA', 'capybara')
+print(string_info('Armageddon'))  # (10, 'ARMAGEDDON', 'armageddon')
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # True ## Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # False # No matches
 
